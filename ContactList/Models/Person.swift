dae3f97643b812bc16d308433c8b440d0ea00de2
vladyslavhaidaienko <gvgnexus5@gmail.com>
2022-02-08 05:24:38 +0200

    first

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,50 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Vladyslav Haidaienko on 08.02.2022.
+//
+
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    private func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(names.count, surnames.count, emails.count, phones.count)
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phone: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
---
+++ ContactList/Services/DataManager.swift
@@ -0,0 +1,39 @@
+//
+//  DataManager.swift
+//  ContactList
+//
+//  Created by Vladyslav Haidaienko on 08.02.2022.
+//
+
+class DataManager {
+    
+    static let shared = DataManager()
+        
+    let names = [
+        "John", "Aaron", "Tim", "Ted",
+        "Steven", "Sharon", "Nicola",
+        "Allan", "Bruce", "Carl"
+    ]
+    
+    let surnames = [
+        "Smith", "Dow", "Isaacson", "Pennyworth",
+        "Jankin", "Butler", "Black", "Robertson",
+        "Murphy", "Williams"
+    ]
+    
+    let emails = [
+        "jjjj@mail.ru", "aaaa@mail.ru", "eeee@mail.ru",
+        "hhhh@mail.ru", "wwww@mail.ru", "mmmm@mail.ru",
+        "xxxx@mail.ru", "pppp@mail.ru", "ssss@mail.ru",
+        "llll@mail.ru"
+    ]
+    
+    let phones = [
+        "745396026", "145036843", "287185656", "293520954",
+        "562880225", "738594072", "825654134", "645898473",
+        "432349845", "459477625"
+    ]
+    
+    private init() {}
+    
+}
